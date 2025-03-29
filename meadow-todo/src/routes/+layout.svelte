6dcefb2ec3a46193375b0c78ab310d304c69b27c
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-03-29 09:56:09 +0800

    todo: ui

--- meadow-todo/src/routes/+layout.svelte
+++ meadow-todo/src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	let { children } = $props();
 </script>
 
-<header class="p-2 flex justify-between items-center bg-green-500">
-	<div class="flex gap-2 justify-center items-center text-green-500">
-		<div class="w-8 h-8 flex justify-center items-center rounded-md bg-green-800">
+<!-- <header class="p-2 flex justify-between items-center text-emerald-950 bg-white">
+	<div class="flex gap-2 justify-center items-center">
+		<div class="w-8 h-8 flex justify-center items-center rounded-md">
 			<span class="material-icons">grass</span> 
 		</div>
 		
@@ -15,6 +15,35 @@
 	<nav>
 		navigation
 	</nav>
-</header>
+</header> -->
 
-{@render children()}
+<nav class="lg:fixed lg:w-20 lg:h-screen">
+	<ul class="lg:h-full lg:flex lg:flex-col lg:items-center">
+		<li class="lg:w-full">
+			<span class="lg:text-[100px] material-icons">home</span>
+		</li>
+
+		<li class="lg:w-full">
+			<span class="material-icons">add_box</span>
+		</li>
+
+		<li class="lg:w-full">
+			<span class="material-icons">calendar_month</span>
+		</li>
+
+		<li class="lg:mt-auto lg:w-full">
+			<!-- <span class="material-icons">light_mode</span> -->
+			<span class="material-icons">dark_mode</span>
+		</li>
+	</ul>
+</nav>
+
+<main class="lg:p-4 lg:ml-20">
+	<h1>This is some HTML BULL</h1>
+	<div class="m-4 w-20 h-20 bg-red-500"></div>
+	<div class="m-4 w-20 h-20 bg-red-500"></div>
+	<div class="m-4 w-20 h-20 bg-red-500"></div>
+	<div class="m-4 w-20 h-20 bg-red-500"></div>
+	<div class="m-4 w-20 h-20 bg-red-500"></div>
+	<!-- {@render children()} -->
+</main>
--- meadow-todo/src/routes/+page.svelte
+++ meadow-todo/src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav class="flex gap-2 justify-start items-center">
+	<p class="px-2 rounded-full bg-white">All</p>
+	<p class="px-2 rounded-full bg-white">Adam</p>
+	<p class="px-2 rounded-full bg-white">Angelia</p>
+</nav>
+
+<section class="grid gap-4 grid-cols-flow">
+	<div class="rounded-lg bg-stone-200 backdrop-blur-lg">
+		<header class="px-2 py-1 flex justify-between items-center">
+			<h1>Task 1</h1>
+			<div class="flex justify-center items-center"><span class="rotate-90 material-icons">chevron_right</span></div>
+		</header>
+		<div class="px-2 py-1 rounded-b-lg bg-stone-300 backdrop-blur-lg">
+			hello world
+		</div>
+	</div>
+
+	<div class="rounded-lg bg-stone-200">
+		<header class="px-2 py-1 flex justify-between items-center">
+			<h1>Task 1</h1>
+			<div class="flex justify-center items-center"><span class="rotate-90 material-icons">chevron_right</span></div>
+		</header>
+		<div class="px-2 py-1 rounded-b-lg bg-stone-300">
+			hello world
+		</div>
+	</div>
+</section>
