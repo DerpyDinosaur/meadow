2f8c925da33b784365ffe4f5d8cda0195db8b57d
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-04-25 14:45:10 +0800

    cleanup script

---
+++ scripts/clean.ts
@@ -0,0 +1,25 @@
+import type { Dirent } from 'node:fs';
+import { readdir, rm } from "node:fs/promises";
+import { join } from 'path';
+
+function isNodeModule(value: Dirent) {
+	return value.isDirectory() && value.name === "node_modules"
+}
+
+async function traverse(dir: string) {
+	return await readdir(dir, { withFileTypes: true, recursive: true });
+}
+
+async function main() {
+	const dir = process.cwd();
+	const root = await traverse(dir)
+	const matched = root.filter(isNodeModule)
+
+	for(const item of matched){
+		const full_path = join(item.parentPath, item.name)
+		console.log("[x]", full_path)
+		await rm(full_path, { recursive: true, force: true });
+	}
+}
+
+await main();
