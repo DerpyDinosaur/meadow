2480ddf446486f40ff21037b3db49c95c8e60293
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-08-28 08:32:05 +0800

    chore: turbo -> nx

--- meadow-todo/src/routes/tasks.remote.ts
+++ meadow-todo/src/routes/tasks.remote.ts
@@ -2,11 +2,11 @@ import { query, form } from "$app/server";
 import client from '$lib/server/api';
 
 export const getTasks = query(async () => {
-	const result = await client.tasks[":id"].$get({
-		param: {
-			id: 1
-		}
-	})
+	// const result = await client.tasks[":id"].$get({
+	// 	param: {
+	// 		id: "1"
+	// 	}
+	// })
 
 	return ["Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading","Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading", "Heading","Heading", "Heading", "Heading", "Heading"]
 })
