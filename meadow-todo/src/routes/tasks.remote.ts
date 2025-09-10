af85f626aa31af276855ba54b94a0d107a9e6302
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-09-11 07:40:45 +0800

    todo: remote function patch added type coercion from hono rpc added

--- meadow-todo/src/routes/tasks.remote.ts
+++ meadow-todo/src/routes/tasks.remote.ts
@@ -1,7 +1,10 @@
-import { query, form, prerender } from "$app/server";
+import { query, form } from "$app/server";
 import client from '$lib/server/api';
+import { fail } from "@sveltejs/kit";
 
-export const get = prerender(async () => {
+// type PatchJsonType = Parameters<typeof client.tasks[":id"]["$put"]>[0]['json'];
+
+export const get = query(async () => {
 	const result = await client.tasks.$get();
 	if(!result.ok){
 		console.error("Unable to fetch tasks");
@@ -13,8 +16,30 @@ export const get = prerender(async () => {
 })
 
 export const patch = form(async (formData) => {
-	console.log(formData)
-	const text = formData.get("text");
+	const id = formData.get("id") as string;
+	const text = formData.get("text") as string;
+	const completed = formData.get("completed") === "on" ? true : false;
+
+	if (!id || !text || completed === null){
+		// fail(400, "ID,Text: Undefined");
+		console.error("ID,Text: Undefined")
+		fail(400, "Shit broke")
+	}
+
+	const result = await client.tasks[":id"].$put({
+		param: { id },
+		json: {
+			text,
+			completed
+		}
+	})
+
+	if(!result.ok){
+		// fail(500, "Broke")
+		console.error("Post failed")
+		fail(400, "Request Shit broke")
+	}
+	await get().refresh();
 })
 
 
