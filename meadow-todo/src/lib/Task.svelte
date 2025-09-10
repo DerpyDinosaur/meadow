9bf8df099078ceac2362045490be3f10573581e2
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-09-11 07:39:38 +0800

    todo: task component created

--- meadow-todo/src/lib/Task.svelte
+++ meadow-todo/src/lib/Task.svelte
@@ -2,23 +2,25 @@
   import type { RemoteForm } from "@sveltejs/kit";
 
   interface Props {
+    id: number;
     text: string;
     patch: RemoteForm<unknown>;
   }
 
-  let { text, patch }: Props = $props();
+  let { id, text, patch }: Props = $props();
   let editing = $state<boolean>(false);
 </script>
 
 {#if editing}
   <form class="p-4 text-2xl neumorphic bg-element rounded-lg" {...patch}>
+    <input hidden name="id" type="number" value={id} />
     <input
       name="title"
       type="text"
-      bind:value={text}
+      value={text}
     />
     <!-- <input bind:value onblur={submit} {required} use:focus/> -->
-    <button class="hidden" type="submit"/>
+    <button type="submit" hidden aria-label="submit"></button>
   </form>
 {:else}
   <div class="p-4 text-2xl neumorphic bg-element rounded-lg" onclick={() => editing=true}>
