7df2e16d23207d0f023e1d0c2e4f57bcc76d7487
DerpyDinosaur <adam.brickhill@lonetree.xyz>
2025-10-21 22:17:56 +0800

    task: completed icon modal: submit on enter

--- meadow-todo/src/lib/Modal.svelte
+++ meadow-todo/src/lib/Modal.svelte
@@ -13,7 +13,7 @@
 
 <div
 	transition:fade={{duration: 200}}
-	class="fixed w-screen h-screen bg-background/50 backdrop-blur-sm" 
+	class="fixed w-screen h-screen bg-background/50 backdrop-blur-sm"
 >
 	<div class="h-full max-h-[66vh] flex justify-center items-center">
 		<div class="p-8 static w-1/2 max-w-3/4 space-y-5 bg-element overflow-auto z-50 neumorphic rounded-lg">
@@ -28,24 +28,25 @@
 			</header>
 
 			<form {...create} class="flex flex-col gap-y-4">
-				<input 
-					name="title" 
-					type="text" 
-					value="" 
-					placeholder="Title" 
+				<input
+					name="title"
+					type="text"
+					value=""
+					placeholder="Title"
 					class="px-4 py-2 neumorphic-inset rounded-2xl"
 				/>
-				<input 
-					name="text" 
-					type="text" 
-					value="" 
-					placeholder="Description" 
+				<input
+					name="text"
+					type="text"
+					value=""
+					placeholder="Description"
 					class="px-4 py-2 neumorphic-inset rounded-2xl"
 				/>
-				<input name="completed" type="checkbox" />
+				<input name="completed" type="checkbox" hidden />
+				<button type="submit" class="invisible">Submit</button>
 			</form>
 		</div>
-		<button 
+		<button
 			onclick={() => history.back()}
 			class="absolute top-0 left-0 w-full h-full z-40"
 			aria-label="close"
--- meadow-todo/src/lib/Task.svelte
+++ meadow-todo/src/lib/Task.svelte
@@ -22,35 +22,9 @@
 <section
 	class="p-4 text-2xl grid grid-cols-[auto_1fr_auto] grid-rows-[1fr_auto] gap-y-2 neumorphic bg-element rounded-lg"
 >
-<!-- 	<header class="grid grid-cols-subgrid col-span-3 space-x-4 items-center">
-		<div class="flex items-center">
-			<div class="m-1 w-5 h-5 ring-2 ring-secondary rounded-full"></div>
-		</div>
-
-		<h2>foo bar</h2>
-
-		<div>
-			<div class="group">
-				<button
-					type="submit"
-					class="p-3 inline-flex justify-center items-center rounded-full neumorphic neumorphic-hover border-border outline-ring/50 bg-element group-hover:text-secondary"
-				>
-					<Check />
-				</button>
-			</div>
-		</div>
-	</header>
-
-	<div class="min-h-10 grid grid-cols-subgrid col-start-2 items-center">
-		<input name="text" type="text" bind:value={formData.text} class="outline-none bg-green-500"/>
-	</div>
-
-	<div class="min-h-10 grid grid-cols-subgrid col-start-2 items-center">
-		<p class="bg-green-500">{formData.text}</p>
-	</div> -->
 	{#if editing}
 		<form
-            {...patch.enhance(async ({ form, submit, data }) => {
+            {...patch.for(task.id).enhance(async ({ form, submit, data }) => {
               await submit();
               editing = false;
             })}
@@ -65,16 +39,25 @@
 			/>
 
 			<header class="grid grid-cols-subgrid col-span-3 space-x-4 items-center">
-				<div class="flex items-center">
-					<div class="m-1 w-5 h-5 ring-2 ring-secondary rounded-full"></div>
-				</div>
+				<button
+				    onclick={() => formData.completed = !formData.completed}
+					type="button"
+					aria-label="Toggle task completion"
+					class="flex items-center"
+				>
+    				<div class="flex items-center">
+                        <div class="relative m-1 w-5 h-5 ring-2 rounded-full {formData.completed ? 'ring-primary text-primary' : 'ring-secondary'}">
+                            <div class:hidden={!formData.completed} class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-primary rounded-full"></div>
+                        </div>
+    				</div>
+				</button>
 
-				<h2>
+				<h2 contenteditable bind:innerText={formData.title}>
 					<input
 						name="title"
 						type="text"
 						bind:value={formData.title}
-						class="p-0 outline-0"
+						hidden
 					/>
 				</h2>
 
@@ -99,9 +82,11 @@
 	{:else}
 		<div class="contents">
 			<header class="grid grid-cols-subgrid col-span-3 space-x-4 items-center">
-				<div class="flex items-center">
-					<div class="m-1 w-5 h-5 ring-2 ring-secondary rounded-full"></div>
-				</div>
+                <div class="flex items-center">
+                    <div class="relative m-1 w-5 h-5 ring-2 rounded-full {formData.completed ? 'ring-primary text-primary' : 'ring-secondary'}">
+                        <div class:hidden={!formData.completed} class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-primary rounded-full"></div>
+                    </div>
+                </div>
 
 				<h2>{formData.title}</h2>
 
